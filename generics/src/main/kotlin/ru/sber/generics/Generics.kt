ab3574bf2df59506442839f5af0f8cb669d0a744
Kirill Sokolinskiy <kirill8256@rambler.ru>
2021-09-25 21:14:35 +0300

    generics done

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,36 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
+//import com.sun.org.apache.xpath.internal.operations.Bool
+//import java.util.*
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <A, B> compare(p1: Pair<A, B>, p2: Pair<A, B>): Boolean {
+    return p1 == p2
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <T : Comparable<T>> countGreaterThan(anArray: Array<T>, elem: T): Int {
+    return anArray.count { it > elem }
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
 }
 
 // 4.
-class Stack {
-
+class Stack<T> {
+    private var stack = ArrayDeque<T>()
+    fun push(element: T) = stack.addFirst(element)
+    fun pop() = stack.removeFirst()
+    fun isEmpty(): Boolean = stack.isEmpty()
+    fun peek() = stack.first()
+    fun search(element: T): T? {
+        return stack.find { it == element }
+    }
 }
