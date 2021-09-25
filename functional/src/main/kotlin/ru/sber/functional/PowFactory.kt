6b4146e5bc9d6dedab9bf519f34e171eca9847bc
Kirill Sokolinskiy <kirill8256@rambler.ru>
2021-09-25 19:12:07 +0300

    functional done

--- functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
+++ functional/src/main/kotlin/ru/sber/functional/PowFactory.kt
@@ -1,10 +1,11 @@
 package ru.sber.functional
 
+import kotlin.math.pow
+
 object PowFactory {
-    /**
-     * Возвращает функцию, которая всегда возводит аргумент в нужную степень, указанную при создании функции.
-     */
-    fun buildPowFunction(pow: Int) {
-        TODO()
+    fun buildPowFunction(pow: Int): (Int) -> Int {
+        return { num ->
+            num.toDouble().pow(pow).toInt()
+        }
     }
 }
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -1,10 +1,23 @@
 package ru.sber.functional
 
+import java.util.function.Predicate
+
 class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+
+    fun filterByPredicate(predicate: (Student) -> (Boolean)): List<Student> {
+        return students.filter { predicate(it) }
+
+    }
+
+    init {
+        students = listOf(
+            Student(firstName = "John", lastName = "Johnson", averageRate = 63.2),
+            Student(firstName = "Alex", lastName = "Blackwood", averageRate = 32.0),
+            Student(firstName = "Peter", lastName = "Jackson", averageRate = 18.1),
+            Student(firstName = "Mike", lastName = "Mikles", averageRate = 92.2)
+        )
     }
 }
